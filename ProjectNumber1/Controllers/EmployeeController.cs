012fc2c97dbb3a2a3df419216834b4c1b955b51f
AhmedGhonim 1998 <aahmed.aashraff1998@gmail.com>
2025-12-17 00:43:10 +0200

    update

--- ProjectNumber1/Controllers/EmployeeController.cs
+++ ProjectNumber1/Controllers/EmployeeController.cs
@@ -1,12 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectNumber1.Models;
+using ProjectNumber1.ViewModel;
 
 namespace ProjectNumber1.Controllers
 {
     public class EmployeeController : Controller
     {
-        public IActionResult Index()
+        
+        ProContext context = new ProContext(); 
+        
+        public IActionResult Details(int id)
         {
-            return View();
+            //this is additionat data not found in the model i want to send it to view
+            string msg = "Hello from action";
+            int temp = 50;
+            List<string> branches = new List<string>();
+            branches.Add("Cairo");
+            branches.Add("Alex");
+            branches.Add("Clifornia");
+            ViewData["Msg"] = msg;
+            ViewData["Temp"] = temp;
+            ViewData["brch"]=branches;
+            ViewBag.Color = "Red";
+            ViewData["Color"] = "Blue";
+
+            Employee empModel = context.Employee.FirstOrDefault(e => e.Id == id);
+            return View("Details" , empModel);
+        }
+
+        public IActionResult DetailsVM(int id)
+        {
+            Employee empModel = context.Employee.Include(e=>e.Departement)
+                .FirstOrDefault(e => e.Id == id);
+            List<string> branches = new List<string>();
+            branches.Add("Cairo");
+            branches.Add("Alex");
+            branches.Add("Clifornia");
+            //declare view model
+            EmpDeptColorTempMsgBrchViewModel EmpVm = new EmpDeptColorTempMsgBrchViewModel();
+            //Mapping
+            EmpVm.EmpName = empModel.Name;
+            EmpVm.DeptName = empModel.Departement.Name;
+            EmpVm.Color = "red";
+            EmpVm.temp = 30;
+            EmpVm.Msg = "Hello from here";
+            EmpVm.Branches = branches;
+            return View("DetailsVM", EmpVm);
+
         }
     }
 }
---
+++ ProjectNumber1/ViewModel/EmpDeptColorTempMsgBrchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ProjectNumber1.ViewModel
+{
+    public class EmpDeptColorTempMsgBrchViewModel
+    {
+        public string EmpName { get; set; }
+        public string DeptName { get; set; }
+        public List<string> Branches { get; set; }
+        public int temp { get; set; }
+        public string Msg { get; set;}
+        public string Color { get; set; }
+    }
+}
